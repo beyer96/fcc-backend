836442e8490afc901bbc317920643a348dfd035c
Lukas Beyer <lukas.beyer@seznam.cz>
2023-07-19 01:05:41 +0200

    Replace yarn plug n play with standard node_modules, cleanup .gitignore and create basic app.js server script

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
