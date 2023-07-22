6e34500a2b4d5d49cb446bc60ea04ad1bf7ad149
Lukas Beyer <lukas.beyer@juicymo.cz>
2023-07-22 23:47:43 +0200

    Define helper methods for encrypting and validating password

---
+++ helpers.js
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import bcrypt from "bcrypt";
+
+export const encryptPassword = async password => {
+  const salt = await bcrypt.genSalt(12);
+  const hash = await bcrypt.hash(password, salt);
+
+  return hash;
+};
+
+export const validPassword = async (password, hashedPassword) => {
+  return bcrypt.compare(password, hashedPassword);
+};
