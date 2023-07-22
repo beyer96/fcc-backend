1da82b243f435f73ba23a24459d7ed61d5afb20a
Lukas Beyer <lukas.beyer@juicymo.cz>
2023-07-22 23:45:26 +0200

    Create registerUser and verifyUser functions

--- database.js
+++ database.js
@@ -1,16 +1,79 @@
 import "dotenv/config";
 import pkg from "pg";
+import bcrypt from "bcrypt";
 
 const { Client } = pkg;
 
-const client = new Client({
+const clientConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
   host: "localhost",
   database: process.env.DB_NAME
-});
+};
 
-client.connect();
+export const getUsers = () => {
+  const client = new Client(clientConfig);
 
-client.end();
+  client.connect();
+  client.query(`
+    SELECT username from users;
+  `);
+};
+
+export const registerUser = async (username, password) => {
+  const client = new Client(clientConfig);
+
+  try {
+    await client.connect();
+
+    const queryText = `
+      INSERT INTO users (
+        username,
+        password,
+        card_ids,
+        created_at
+      )
+      VALUES (
+        $1,
+        $2,
+        ARRAY[]::int[],
+        CURRENT_TIMESTAMP
+      ) ON CONFLICT (username) DO NOTHING;
+    `;
+    const values = [username, password];
+    const result = await client.query(queryText, values);
+
+    if (result.rowCount > 0) {
+      console.log(`User ${username} successfully created!`);
+    } else {
+      console.log(`User ${username} already exists!`);
+    }
+  } catch (err) {
+    console.log(`User ${username} could not be saved to the database: ${err.message}`);
+  } finally {
+    client.end();
+  }
+};
+
+export const verifyUser = async (username, password) => {
+  const client = new Client(clientConfig);
+
+  try {
+    const queryText = `SELECT * FROM users WHERE username = $1;`;
+
+    await client.connect();
+    const user = await client.query(queryText, [username]);
+    if (user.rowCount == 0) return false;
+
+    const hashedPassword = user.rows[0].password;
+    const isPasswordValid = await bcrypt.compare(password, hashedPassword);
+
+    return isPasswordValid;
+  } catch (err) {
+    console.log(err.message);
+  } finally {
+    client.end();
+  }
+};
+
