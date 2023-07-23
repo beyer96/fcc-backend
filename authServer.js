08e7784bbc6a6fd93bc9c4a644e19b974ac05f8d
Lukas Beyer <lukas.beyer@juicymo.cz>
2023-07-23 11:33:11 +0200

    Move helpers.js in helpers folder and rename to auth.js

--- authServer.js
+++ authServer.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import express from "express";
 import jwt from "jsonwebtoken";
 
-import { encryptPassword } from "./helpers.js"; 
+import { encryptPassword } from "./helpers/auth.js"; 
 import { registerUser, verifyUser } from "./database.js";
 
 const app = express();
